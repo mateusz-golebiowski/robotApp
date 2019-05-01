e2b1935773e985043c490e4af1418e502fe45f32
Mateusz Gołębiowski <golebiowski.mateusz@outlook.com>
2019-05-01 13:31:16 +0200

    add address form

--- src/App.js
+++ src/App.js
@@ -6,37 +6,15 @@ import io from 'socket.io-client';
 
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = {connected: false};
+ 
+        this.connect = this.connect.bind(this);
+    }
   componentDidMount() {
-    const socket = io('http://192.168.1.206');
     
-    socket.emit('register controller');
-
-    const options = {
-        zone: document.getElementById('joyStick'),
-        color: 'blue',
-        mode: 'static',
-        position: {left: '50%', top: '50%'},
-    };
-    const manager = nipplejs.create(options);
-    manager.on('move', (evt, data) => {
-        console.log({
-            distance: data.distance,
-            angle: data.angle
-
-            });
-        socket.emit('move device', {
-            distance: data.distance,
-            angle: data.angle
-
-            });
-    
-    }).on('end', (evt, data)=>{
-        socket.emit('move device',  {
-            distance: data.distance,
-            angle: data.angle
-
-            });
-    });
         console.log('mount it!');
         
   };
@@ -44,24 +22,82 @@ class App extends Component {
   clicked(e) {
       console.log('You clicked me');
   }
+
+  connect(e){
+    e.preventDefault();
+    this.setState({connected: true});
+
+
+    console.log(e.target.ip.value);
+    const socket = io(e.target.ip.value);
+    socket.emit('register controller');
+
+    socket.on('connect_error', function() {
+
+        console.log("Sorry, there seems to be an issue with the connection!");
+        socket.close();
+    })
+    socket.on('connect', function() {
+        const x = document.getElementsByClassName("grid-container")[0];
+        x.style.display = "grid";
+    
+        const y =  document.getElementById("form");
+        y.style.display = "none";
+        const options = {
+            zone: document.getElementById('joyStick'),
+            color: 'blue',
+            mode: 'static',
+            position: {left: '50%', top: '50%'},
+        };
+        const manager = nipplejs.create(options);
+        manager.on('move', (evt, data) => {
+            console.log({
+                distance: data.distance,
+                angle: data.angle
+
+                });
+            socket.emit('move device', {
+                distance: data.distance,
+                angle: data.angle
+
+                });
+        
+        }).on('end', (evt, data)=>{
+            socket.emit('move device',  {
+                distance: data.distance,
+                angle: data.angle
+
+                });
+        });
+     });
+
+
+    
+    
+  }
   render() {
+
     return (
         <div>
+            <form onSubmit={this.connect} id="form">
+                <label htmlFor="ip">Ip</label><input type="text" id="ip" name="ip"></input>
+            </form>
             <div id='id' className="grid-container">
                 <div className="up">
-                    <button onClick={this.clicked}><i class="upButton"></i></button>
+                    <button onClick={this.clicked}><i className="upButton"></i></button>
                 </div>
                 <div className="left">
-                    <button onClick={this.clicked}><i class="leftButton"></i></button>
+                    <button onClick={this.clicked}><i className="leftButton"></i></button>
                 </div>
                 <div className="pad" id='joyStick'></div>
                 <div className="right">
-                    <button onClick={this.clicked}><i class="rightButton"></i></button>
+                    <button onClick={this.clicked}><i className="rightButton"></i></button>
                 </div>
                 <div className="down">
-                    <button onClick={this.clicked}><i class="downButton"></i></button>
+                    <button onClick={this.clicked}><i className="downButton"></i></button>
                 </div>
             </div>
+
         </div>
     );
   }
