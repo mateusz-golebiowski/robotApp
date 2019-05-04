0d178049ce9a526226cf04253c11d67f44dcb57f
Mateusz Gołębiowski <golebiowski.mateusz@outlook.com>
2019-05-04 18:17:08 +0200

    add moving robot by keyboard

--- src/JoyStick.jsx
+++ src/JoyStick.jsx
@@ -3,6 +3,7 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import nipplejs from 'nipplejs';
+import KeyHandler, { KEYPRESS, KEYUP } from 'react-key-handler';
 
 const styles = theme => ({
     controlPanel: {
@@ -96,10 +97,81 @@ class JoyStick extends Component {
         this.props.onMove(data);            
     }
     
+    handleKeyClicked = (event)=> {
+        
+        let move;
+        if(event.key==='w')
+            move= { speedLeft: 255, speedRight: 255, direction: "up" };
+        else if(event.key === 's')
+            move= { speedLeft: 255, speedRight: 255, direction: "down" };
+        else if(event.key === 'a')
+            move= { speedLeft: 255, speedRight: 0, direction: "up" };
+        else if(event.key === 'd')
+            move= { speedLeft: 0, speedRight: 255, direction: "up" };
+
+        if(!this.isEqual(this.state.lastOutput,move)){
+            console.log(move);
+            this.handleMove(move);
+            this.setState({lastOutput: move});
+        }
+        
+    }
+    handleKeyUp = (event)=> {
+        
+        const move= { speedLeft: 0, speedRight: 0, direction: "up" };
+        
+        if(!this.isEqual(this.state.lastOutput,move)){
+            console.log(move);
+            this.handleMove(move);
+            this.setState({lastOutput: move});
+        }
+    }
     render() {
         const { classes } = this.props;
         return (
+            
             <div id='ControlPanel' className={classes.controlPanel}>
+                <KeyHandler
+                    keyEventName={KEYPRESS}
+                    keyValue="w"
+                    onKeyHandle={this.handleKeyClicked}
+                />
+                <KeyHandler
+                    keyEventName={KEYPRESS}
+                    keyValue="s"
+                    onKeyHandle={this.handleKeyClicked}
+                />
+                <KeyHandler
+                    keyEventName={KEYPRESS}
+                    keyValue="a"
+                    onKeyHandle={this.handleKeyClicked}
+                />
+                <KeyHandler
+                    keyEventName={KEYPRESS}
+                    keyValue="d"
+                    onKeyHandle={this.handleKeyClicked}
+                />
+                <KeyHandler
+                    keyEventName={KEYUP}
+                    keyValue="w"
+                    onKeyHandle={this.handleKeyUp}
+                />
+                <KeyHandler
+                    keyEventName={KEYUP}
+                    keyValue="s"
+                    onKeyHandle={this.handleKeyUp}
+                />
+                <KeyHandler
+                    keyEventName={KEYUP}
+                    keyValue="a"
+                    onKeyHandle={this.handleKeyUp}
+                />
+                <KeyHandler
+                    keyEventName={KEYUP}
+                    keyValue="d"
+                    onKeyHandle={this.handleKeyUp}
+                />
+
                 <div className={classes.pad} id='joyStick'></div>
             </div>
         );
